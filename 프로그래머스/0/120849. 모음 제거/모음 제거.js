0325d995d2d66a9dbc067f35544c702e8219af2f
Sapient92 <151002897+Sapient92@users.noreply.github.com>
2024-02-26 14:12:00 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    var answer = my_string.replaceAll("a", "")
+  .replaceAll("e", "")
+  .replaceAll("i", "")
+  .replaceAll("o", "")
+  .replaceAll("u", "");;
+    return answer;
+}
