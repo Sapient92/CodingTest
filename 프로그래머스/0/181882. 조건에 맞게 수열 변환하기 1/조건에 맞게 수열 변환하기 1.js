316ccd8458dc10ca98ce6103483ac38de12adcfb
Sapient92 <151002897+Sapient92@users.noreply.github.com>
2024-02-26 17:50:27 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 31.05 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    var answer = arr.map(el => {
+        if(el >= 50 && !(el % 2)) {
+            return el/2
+        } else if(el < 50 && (el % 2)) {
+            return el * 2
+        } else {
+            return el;
+        }
+    });
+    return answer;
+}
