a3a7c02b47f171f74531a0634de12a3ad50331e8
Sapient92 <151002897+Sapient92@users.noreply.github.com>
2024-03-05 10:07:51 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,9 +1,4 @@
 function solution(n_str) {
-    var answer = '';
-    for(let i = 0; i < n_str.length; i++) {
-        if(n_str[i] !== "0") {
-            return n_str.substring(i)
-        }
-    }
+    var answer = +n_str+""
     return answer;
 }
