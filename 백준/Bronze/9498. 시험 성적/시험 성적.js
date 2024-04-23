02dab6501563b24e1d66ba535c1f805742404202
Sapient92 <151002897+Sapient92@users.noreply.github.com>
2024-04-23 19:40:13 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -1,26 +1,15 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString().split("\n");
+let data = input[0].split(" ")[0];
 
-let input = [];
-
-readline.on('line', function(line) {
-    input = line.split(' ').map(el => parseInt(el));
-}).on('close', function(){
-	const A = parseInt(input[0]);
-    if ( A=>0 && A <= 100) {
-        if (A >= 90) {
-            console.log('A');
-        } else if (A >= 80) {
-            console.log('B');
-        } else if (A >= 70) {
-            console.log('C');
-        } else if (A >= 60) {
-            console.log('D');
-        } else {
-            console.log('F');
-        }
-    }
-    process.exit();
-});
+if (data >= 90) {
+  console.log("A");
+} else if (data >= 80 && data <= 89) {
+  console.log("B");
+} else if (data >= 70 && data <= 79) {
+  console.log("C");
+} else if (data >= 60 && data <= 69) {
+  console.log("D");
+} else {
+  console.log("F");
+}
